ba18f5dcfd6e389b24fa3155c31db0ee98a4ac34
MohammedHaidous <Mohammh@umich.edu>
2024-11-19 23:19:51 -0500

    intial start to server.js

--- agentconnect-backend/server.js
+++ agentconnect-backend/server.js
@@ -1,1 +1,17 @@
-console.log("Test")
+//Constants
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+
+
+//root directory for REST request
+app.get('/', (req, res) => {
+    res.send("Hello node and express API");
+}); 
+
+app.listen(PORT, () => {
+    console.log(`Node API app is running on port ${PORT}`);
+});
