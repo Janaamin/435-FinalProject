224023c7377c8ce6ba3884fb389133dddf780f01
MohammedHaidous <Mohammh@umich.edu>
2024-11-20 23:33:37 -0500

    Added user endpoints

---
+++ agentconnect-backend/controllers/userController.js
@@ -0,0 +1,56 @@
+const User = require('../models/user');
+
+
+//Fetch all agents
+const getAllAgents = async (req, res) => {
+    try{
+        const agents = await User.find({role: 'agent'}).select('-password');
+        res.status(200).json(agents);
+    } catch (error){
+        console.error('Get all agents error:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+}
+
+//Fetching user profile
+const getUserProfile = async (req, res) => {
+    try {
+      const userId = req.user.userId; // Extract user ID from decoded token
+  
+      const user = await User.findById(userId).select('-password'); // Fetch user, exclude password
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      res.status(200).json(user);
+    } catch (error) {
+      console.error('Get profile error:', error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  };
+
+//Update user profile
+const updateUserProfile = async (req, res) => {
+    try {
+      const userId = req.user.userId; // Extract user ID from decoded token
+      const updates = req.body; // Get updates from the request body
+  
+      const updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { $set: updates },
+        { new: true, runValidators: true } // Return the updated document and validate inputs
+      ).select('-password'); // Exclude password from the response
+  
+      if (!updatedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      console.error('Update profile error:', error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  };
+  
+  module.exports = { getUserProfile, updateUserProfile, getAllAgents };
+  
